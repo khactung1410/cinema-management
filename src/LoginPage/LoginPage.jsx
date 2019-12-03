6cfcfa12f3f617d92bf9ecf1923f10bb0d3e3fb7
Tung_DK <tung_dk@septeni-technology.jp>
2019-12-03 18:35:09 +0700

    add new Login Interface

--- src/LoginPage/LoginPage.jsx
+++ src/LoginPage/LoginPage.jsx
@@ -24,6 +24,7 @@ class LoginPage extends React.Component {
     }
 
     handleSubmit = e => {
+        console.log(111111111111)
         e.preventDefault();
         this.setState({ submitted: true });
         const { username, password } = this.state;
@@ -36,6 +37,7 @@ class LoginPage extends React.Component {
         const { loggingIn } = this.props;
         const { username, password, submitted } = this.state;
         return (
+            <div>
             <div className="col-md-4 col-md-offset-3 container">
                 <div className="container">
                     <h2 style={{margin: "0 90px"}}>Cinema Management</h2>
@@ -64,6 +66,37 @@ class LoginPage extends React.Component {
                         <Link to="/register" className="btn btn-link">Register</Link>
                     </div>
                 </form>
+            </div>
+            <div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                            <div className="card card-signin my-5">
+                                <div className="card-body">
+                                    <h5 className="card-title text-center">Sign In</h5>
+                                    <form className="form-signin" onSubmit={this.handleSubmit}>
+                                        <div className="form-label-group">
+                                            <input type="text" id="inputEmail" className="form-control" name="username" value={username} placeholder="Username" required autoFocus onChange={this.handleChange}/>
+                                            <label htmlFor="inputEmail">Username</label>
+                                            {submitted && !username &&
+                                            <div className="help-block">Username is required</div>
+                                            }
+                                        </div>
+                                        <div className="form-label-group">
+                                            <input type="password" id="inputPassword" className="form-control" name="password" placeholder="Password" required onChange={this.handleChange}/>
+                                            <label htmlFor="inputPassword">Password</label>
+                                            {submitted && !password &&
+                                            <div className="help-block">Username is required</div>
+                                            }
+                                        </div>
+                                        <button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             </div>
         );
     }
